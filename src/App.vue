69a61c7879d2985c57f26944f40025cb5d5dab4d
Abduvohid <vohid4970@gmail.com>
2022-09-21 10:48:25 +0500

    display-grid

--- src/App.vue
+++ src/App.vue
@@ -7,12 +7,14 @@ export default {};
 </script>
 
 <style>
+
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  display: grid;
 }
 @font-face {
   font-family: "SF UI Display";
